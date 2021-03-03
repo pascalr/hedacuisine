3aba3179f7df17ceb8868128afaac21828308b3f
Dark Krael <dark.krael@gmail.com>
2021-03-03 13:21:21 -0500

    .

--- app/models/unit_system.rb
+++ app/models/unit_system.rb
@@ -1,4 +1,8 @@
 class UnitSystem < ApplicationRecord
   has_many :unit_system_items
   has_many :units, through: :unit_system_items
+
+  def self.default
+    @default ||= UnitSystem.first # TODO: Use order
+  end
 end
