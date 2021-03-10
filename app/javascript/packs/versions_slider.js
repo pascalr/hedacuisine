433cf522b655dccce4f31e70d740a09134e803a7
Dark Krael <dark.krael@gmail.com>
2021-03-10 11:27:57 -0500

    .

--- app/javascript/packs/versions_slider.js
+++ app/javascript/packs/versions_slider.js
@@ -4,11 +4,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var slider = tns({
     container: "#versions-slider",
-    items: 2,
-    slideBy: 'page',
-    autoWidth: true,
+    items: 1,
+    //autoWidth: true,
     //gutter: 10,
     nav: false,
+    center: true,
+    loop: false,
+    swipeAngle: false,
+    speed: 400
     //prevButton: document.getElementById('prev-tag-'+elements[i].dataset.tagid),
     //nextButton: document.getElementById('next-tag-'+elements[i].dataset.tagid),
     //lazyload: true,
