5253a8185519207ff00d38f704fe9d3e0916862e
Dark Krael <dark.krael@gmail.com>
2021-03-12 14:47:22 -0500

    .

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -28,7 +28,7 @@ class MenusController < ApplicationController
 
   def add_or_create_recipe
     category = Category.find params[:category_id]
-    recipe = Recipe.find params[:recipe_id]
+    recipe = Recipe.find_by id: params[:recipe_id]
     ActiveRecord::Base.transaction do
       recipe = Recipe.create!(name: params[:recipe_name], source: params[:recipe], user: current_user) unless recipe
       Item.create!(category: category, menu: category.menu, recipe: recipe)
