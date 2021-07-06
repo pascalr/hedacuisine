70e822a9ea34469b7d512ccda3241a4fbded7738
Dark Krael <dark.krael@gmail.com>
2021-07-06 16:08:22 -0400

    .

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,9 @@ class SearchController < ApplicationController
   def index
     menu = Menu.find_by(name: params[:search])
     redirect_to menu if menu
+    recipe = Recipe.where('lower(name) = ?', params[:search]).first 
+    redirect_to recipe if recipe
+    @recipes = Recipe.where("lower(name) like ?", "%#{params[:search]}%")
+    @results = @recipes
   end
 end
