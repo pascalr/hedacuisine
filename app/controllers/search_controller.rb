2e939d82c5f4f1987ce3909697b45c6a22cd224d
Dark Krael <dark.krael@gmail.com>
2021-08-28 09:19:08 -0400

    .

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,8 @@ class SearchController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :only_admin!
   def index
+    @foods= Food.where("lower(name) like ? OR lower(plural) like ?", "%#{params[:search]}%", "%#{params[:search]}%")
     @recipes = Recipe.where("lower(name) like ?", "%#{params[:search]}%")
-    @results = @recipes
+    @results = @recipes + @foods
   end
 end
