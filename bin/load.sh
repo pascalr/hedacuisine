bada918b1ac174db50ddaf42d6159f8336f6b0e6
Dark Krael <dark.krael@gmail.com>
2021-08-29 09:32:50 -0400

    .

--- bin/load.sh
+++ bin/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #pg_restore -d hedacuisine_development latest.dump
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U hedacuisine -d hedacuisine_development latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U hedacuisine -d hedacuisine_development $1
