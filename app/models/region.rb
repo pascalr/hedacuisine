aa90b76b7cf058fb97780ec43f48d1d2d679a934
Dark Krael <dark.krael@gmail.com>
2021-10-26 14:15:46 -0400

    .

--- app/models/region.rb
+++ app/models/region.rb
@@ -5,4 +5,10 @@ class Region < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :locale, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
+
+  def locale_with_underscore
+    l = locale
+    l[2] = "_" if l[2] == "-"
+    l
+  end
 end
