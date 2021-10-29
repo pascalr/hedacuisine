a3e1ca6058149d1ffb0e156215d2e240d05eecfa
Dark Krael <dark.krael@gmail.com>
2021-10-29 17:11:04 -0400

    .

--- app/javascript/packs/whole_page_search.js
+++ app/javascript/packs/whole_page_search.js
@@ -3,18 +3,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //var container = document.getElementById('top-search-container')
   //var container = document.getElementById('search-image')
 
+  var container = $('#top-search-container')
+  var whole = $("#whole-page-search")
+  var searchResults = $('#search-results')
+
   /* FIXME: Accessible from keyboard only */
-  $('.top-search-container').on('click', (evt) => {
-  //$('.search-image').on('click', (evt) => {
+  container.on('click', (evt) => {
     console.log('clicked')
-    $("#whole-page-search").slideDown("slow", function() {
+    //container.hide();
+    whole.slideDown();
+    //$("#top-search").animate({translateY: "-500px"});
+    //container.css("transform", "translateY(20vh) !important");
+    //container.css("margin-top", "3rem");
+    //container.css("width", "80%");
+    //container.css("background-color", "red")
+    //container.width("100vw");
+    //container.height("100vh");
+    //container.css("transform", "translateX(0)");
+    //$("#whole-page-search").slideDown("slow", function() {
+    //container.slideDown("slow", function() {
       // Animation complete.
-    });
+    //});
   });
-  $('.close-search').on('click', (evt) => {
-    console.log('clicked')
-    $("#whole-page-search").slideUp()
+  container.on('focusout', (evt) => {
+    whole.slideUp()
   });
+  url = "/fr/recettes.json" // FIXME: Pass this as a data attribute, this way it handles the language too.
+  $.get(url, function(data) {
+    console.log("data:", data)
+    data.forEach(recipe => {
+      searchResults.append($('<li><a href="'+recipe.url+'">'+'<img src="'+recipe.image+'"></img>'+recipe.name+'</a></li>'))
+    })
+  });
+  $.ajax({
+    type: "GET",
+    url: url,
+  });
+  //$('.close-search').on('click', (evt) => {
+  //  console.log('clicked')
+  //  whole.slideUp()
+  //});
 
   /*
   var elem = document.getElementById('whole-page-search')
--- app/views/recipes/index.json.jbuilder
+++ app/views/recipes/index.json.jbuilder
@@ -1,1 +1,6 @@
-json.array! @recipes, partial: "recipes/recipe", as: :recipe
+# json.array! @recipes, partial: "recipes/recipe", as: :recipe
+json.array! @recipes do |recipe|
+  json.name recipe.name
+  json.url recipe_path(recipe)
+  json.image thumb_image_path(recipe.image)
+end
