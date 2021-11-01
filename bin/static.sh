a11fecfefd6dc0c5f72b8e4d3485630b440b01a3
Dark Krael <dark.krael@gmail.com>
2021-11-01 16:16:41 -0400

    .

--- bin/static.sh
+++ bin/static.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#python3 -m http.server --directory "tmp/localhost:3000"
-python3 bin/server_static.py 8000 --directory "tmp/localhost:3000"
+
+# TODO: backup, sync, load latest backup, check warnings, build
