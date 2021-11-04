77304b7b56353354d1b1cb0200e9e1050f3a80b5
Dark Krael <dark.krael@gmail.com>
2021-11-04 16:14:12 -0400

    .

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -5,11 +5,13 @@ class UserRecipesController < ApplicationController
   before_action :set_user_recipes, only: [:index, :index_with_pictures, :index_edit, :index_with_details]
 
   def index
+    # OPTIMIZE: Using paginage for this is too much for nothing I believe
     @recipes = current_user.recipes.paginate(page: params[:page], per_page: 1)
     @recipe = @recipes.first
   end
 
   def show_recipe_page
+    # OPTIMIZE: Using paginage for this is too much for nothing I believe
     @recipes = current_user.recipes.paginate(page: params[:page], per_page: 1)
     @recipe = @recipes.first
     render partial: "user_recipes/current_page", locals: {recipe: @recipe}, layout: nil
