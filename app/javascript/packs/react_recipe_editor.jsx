cfa5f4426fdd5966feb9025017eaf8a668b72778
Dark Krael <dark.krael@gmail.com>
2021-11-19 11:07:18 -0500

    .

--- app/javascript/packs/react_recipe_editor.jsx
+++ app/javascript/packs/react_recipe_editor.jsx
@@ -261,39 +261,6 @@ const InstructionsShortcuts = props => (
     </div>
   </>
 )
-const InstructionsHelp = props => (
-  <>
-    <button type="button" className="btn-image">
-      <img src="/icons/question-circle-blue.svg" data-bs-toggle="collapse" data-bs-target="#show-help" style={{width: "2em"}}></img>
-    </button>
-    <div className="collapse" id="show-help">
-      <h2>Aide</h2>
-      <h3>Instructions</h3>
-      <ul>
-        <li><b>#</b>: Commencer une ligne avec «#» pour chaque étape de la recette.</li>
-        <li><b>$</b>: Commencer une ligne avec «$» pour un grand titre.</li>
-        <li><b>$$</b>: Commencer une ligne avec «$$» pour un moyen titre.</li>
-        <li><b>$$$</b>: Commencer une ligne avec «$$$» pour un petit titre.</li>
-        <li><b>/</b>: Commencer une ligne avec «/» pour faire un paragraph en italique.</li>
-        <li><b>{"{3}"}</b>: Afficher l'ingrédient 3</li>
-        <li><b>{"{3-5}"}</b>: Afficher les ingrédients 3, 4 et 5</li>
-        <li><b>{"{3,5}"}</b>: Afficher les ingrédients 3 et 5</li>
-        <li><b>{"{3;}"}</b>: TODO: Afficher le nombre 3 qui scale avec la recette</li>
-        <li><b>{"{3;pomme}"}</b>: TODO: Afficher la quantité 3 pomme qui scale avec la recette</li>
-        <li><b>{"{2;pomme,3-5}"}</b>: TODO: Afficher la quantité 3 pomme qui scale avec la recette et les ingrédients 3, 4 et 5.</li>
-        <li><b>[1]</b>: TODO: Lien vers la note 1</li>
-        <li><b>[1;Section name]</b>: TODO: Lien vers la section de l'article</li>
-        <li><b>[note: 1]</b></li>
-        <li><b>[link_note: 1]</b></li>
-        <li><b>[recipe: 100]</b></li>
-        <li><b>[food: 100]</b></li>
-        <li><b>[url: "http://www.hedacuisine.com/"]</b></li>
-        <li><b>[label: "home", url: "http://www.hedacuisine.com/"]</b></li>
-        <li><b>[img: 10]</b></li>
-      </ul>
-    </div>
-  </>
-)
 
 const NewIngInputField = props => {
 
