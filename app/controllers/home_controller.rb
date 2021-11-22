6dff1e177075de799766a27f3b2e208455658cb3
Dark Krael <dark.krael@gmail.com>
2021-11-22 14:23:30 -0500

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   skip_before_action :only_admin!
   def index
     @kinds = Kind.all
+    @recipes_by_kind = @kinds.map {|k| [k, k.recipes.all_main.all_public.with_images.limit(10)] }.sort_by {|(k, rs)| rs.size }.reverse
   end
   def beta
   end
