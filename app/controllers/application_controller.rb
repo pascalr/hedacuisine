8fde40c729cdbcef855d87659adf20ec14d60ded
Dark Krael <dark.krael@gmail.com>
2022-01-22 09:52:18 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_recipes_path
+    my_recipes_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
