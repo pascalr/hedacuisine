9fec100381344c2dc6d38e7221c00880970b0334
Dark Krael <dark.krael@gmail.com>
2022-02-27 09:01:13 -0500

    .

--- app/javascript/packs/book_sidebar.js
+++ app/javascript/packs/book_sidebar.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 
 import { isBlank, normalizeSearchText } from "../utils"
 
+import Hammer from "react-hammerjs"
+
 const BookSidebar = () => {
   
   const collapseElem = useRef(null);
@@ -11,7 +13,9 @@ const BookSidebar = () => {
   const [data, setData] = useState(null)
   const [search, setSearch] = useState('')
   const [selected, setSelected] = useState(-1)
-  
+  const [visible, setVisible] = useState(false)
+ 
+  // TODO: Use useFetch instead of this const data = (init) ? useFetch(...) : null
   useEffect(() => {
     if (init) {
       $.get(collapseElem.current.parentNode.dataset.url, function(data) {
@@ -26,25 +30,21 @@ const BookSidebar = () => {
   }
   
   useEffect(() => {
+    let elems = document.querySelectorAll('[data-book-search-toggle]')
+    for (let i = 0; i < elems.length; i++) {
+      let elem = elems[i]
+      elem.handleToggle = () => {setVisible(true)} // FIXME: This does not work: setVisible(!visible)
+      elem.addEventListener("click", elem.handleToggle)
+    }
     collapseElem.current.addEventListener("shown.bs.collapse", doInit)
     return () => {
+      let elems = document.querySelectorAll('[data-book-search-toggle]')
+      for (let i = 0; i < elems.length; i++) {
+        elem.removeEventListener("click", elem.handleToggle)
+      }
       collapseElem.current.removeEventListener("", doInit)
     }
   }, []);
-
-
-  //  renderItem: function ({section, recipe}, search){
-  //    search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-  //    var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-  //    let r = ""
-  //    if (section) {r += `<h3>${section.name}</h3>`}
-  //    return r+'<a class="autocomplete-suggestion" data-val="'+recipe.recipe.name+'" href="'+recipe.url+'"><b>' + recipe.recipe.name + '</b></a>'
-  //  },
-  //  onSelect: function(e, term, item){
-  //    window.location.href = item.href
-  //  }
-  //})
-  //inputField.current.showEmptyAutocomplete()
  
   let term = normalizeSearchText(search)
   let filtered = []
@@ -65,32 +65,36 @@ const BookSidebar = () => {
     if (key == "ArrowDown") {select(selected >= filtered.length-1 ? -1 : selected+1)}
     if (key == "ArrowUp") {select(selected < 0 ? filtered.length-1 : selected-1)}
     if (key == "Enter") {window.location.href = filtered[selected].url+"#main"}
-          
   }
-  console.log('filtered', filtered)
-  console.log('selected', selected)
+
+  let handleSwipe = ({direction}) => {
+    if (direction == 2) {
+      setVisible(false)
+    }
+  }
 
   let sectionPrinted = {}
-
   return (<>
-    <div id="search-book" ref={collapseElem} className="collapse collapse-horizontal" style={{border: "1px solid black", padding: "0.5em", height: "100%"}}>
-      <div style={{width: "300px", height: "100%"}}>
-        <h2 style={{fontSize: "1.5em"}}>{data ? data.book_name : ''}</h2>
-        <input id="book-filter" type="search" placeholder="Filtrer..." onChange={(e) => setSearch(e.target.value)} autoComplete="off" style={{width: "100%"}} onKeyDown={onKeyDown}/>
-        {filtered.map((book_recipe, current) => {
-          let printSection = (section_id) => {
-            if (sectionPrinted[section_id]) {return ''}
-            sectionPrinted[section_id] = true
-            return <h3>{data.book_sections.find(b => b.id == section_id).name}</h3>
-          }
-          return (
-            <div key={book_recipe.id}>
-              {printSection(book_recipe.book_section_id)}
-              <a className={current == selected ? "selected" : undefined} href={book_recipe.url+"#main"}>{book_recipe.recipe.name}</a>
-            </div>
-          )
-        })}
-      </div>
+    <div id="search-book" ref={collapseElem} style={{height: "100%", position: "relative", left: (visible ? "0px" : "-300px"), transition: "transform .5s ease-out"}}>
+      <Hammer onSwipe={handleSwipe}>
+        <div style={{border: "1px solid black", padding: "0.5em", width: "300px", height: "100%"}}>
+          <h2 style={{fontSize: "1.5em"}}>{data ? data.book_name : ''}</h2>
+          <input id="book-filter" type="search" placeholder="Filtrer..." onChange={(e) => setSearch(e.target.value)} autoComplete="off" style={{width: "100%"}} onKeyDown={onKeyDown}/>
+          {filtered.map((book_recipe, current) => {
+            let printSection = (section_id) => {
+              if (sectionPrinted[section_id]) {return ''}
+              sectionPrinted[section_id] = true
+              return <h3>{data.book_sections.find(b => b.id == section_id).name}</h3>
+            }
+            return (
+              <div key={book_recipe.id}>
+                {printSection(book_recipe.book_section_id)}
+                <a className={current == selected ? "selected" : undefined} href={book_recipe.url+"#main"}>{book_recipe.recipe.name}</a>
+              </div>
+            )
+          })}
+        </div>
+      </Hammer>
     </div>
   </>)
 }
