aacc2e1df64221e25bd268a15ef29613b4010fbc
Dark Krael <dark.krael@gmail.com>
2022-03-08 18:50:26 -0500

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// I don't know how this works but this works well.
+// It will bind js to all elements with attribute data-bs-toggle"..."
+import * as bootstrap from 'bootstrap' // OPTIMIZE: Don't import everything?
