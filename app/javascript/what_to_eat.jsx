519ff8fceeb39e611337c3abe912e95d368bff43
Pascal Rainville <pascal.rainville@gmail.com>
2022-04-01 11:34:39 -1000

    .

--- app/javascript/what_to_eat.jsx
+++ app/javascript/what_to_eat.jsx
@@ -6,13 +6,13 @@ const ChooseOccasion = () => {
   return (<>
     <h2 style={{textAlign: "center"}}>Pour quelle occasion cuisiner?</h2>
     <div id="occasions-btns" className="d-flex" style={{flexDirection: "column"}}>
-      <button type="button" className="btn btn-primary">Se gâter</button>
       <button type="button" className="btn btn-primary">Repas pour toute la semaine</button>
-      <button type="button" className="btn btn-primary">Repas pour emporter la journée</button>
-      <button type="button" className="btn btn-primary">Pour passer un ingrédient</button>
-      <button type="button" className="btn btn-primary">Pour recevoir des invités</button>
-      <button type="button" className="btn btn-primary">Pour apporter à un potluck</button>
-      <button type="button" className="btn btn-primary">Une recette rapide de semaine</button>
+      <button type="button" className="btn btn-primary">Recette rapide de semaine</button>
+      <button type="button" className="btn btn-primary">Recevoir des invités</button>
+      <button type="button" className="btn btn-primary">Apporter à un potluck</button>
+      <button type="button" className="btn btn-primary">Emporter un repas pour la journée</button>
+      <button type="button" className="btn btn-primary">Un ingrédient près d'être périmé</button>
+      <button type="button" className="btn btn-primary">Se gâter</button>
     </div>
   </>)
 }
