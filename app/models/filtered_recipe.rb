fed81e06043f0d0c4dc5c611afeab0795472b2ec
Pascal Rainville <pascal.rainville@gmail.com>
2022-04-18 10:16:16 -1000

    .

--- app/models/filtered_recipe.rb
+++ app/models/filtered_recipe.rb
@@ -1,4 +1,8 @@
 class FilteredRecipe < ApplicationRecord
   belongs_to :recipe_filter
   belongs_to :filterable, polymorphic: true
+
+  validates :recipe_filter_id, uniqueness: { scope: [:filterable_id, :filterable_type],
+    message: "should only be one filtered recipe by item" }
+
 end
