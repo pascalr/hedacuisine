bba62ba407d12230a53da2972308ca0df757877d
Pascal Rainville <pascal.rainville@gmail.com>
2022-04-18 18:55:08 -1000

    .

--- app/models/favorite_recipe.rb
+++ app/models/favorite_recipe.rb
@@ -1,4 +1,13 @@
+class FavoriteRecipeValidator < ActiveModel::Validator
+  def validate(record)
+    if record.recipe.user_id == record.user_id
+      record.errors.add :base, "A user can't have his own recipe as a favorite"
+    end
+  end
+end
+
 class FavoriteRecipe < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
+  validates_with FavoriteRecipeValidator
 end
